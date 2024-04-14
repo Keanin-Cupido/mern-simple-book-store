7bf1d09f8be5115ac57d78706f4784d731bb13c0
Keanin Cupido <keanin16@gmail.com>
2024-04-14 22:15:23 +0200

    add mongodb and mongoose

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:hRBf9RPG0AyMMfmn@mern--simple-book-store.xzj58hh.mongodb.net/books-collection?retryWrites=true&w=majority';
